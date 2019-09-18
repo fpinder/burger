693765ce8f6cab1777625c42e683169ff37226d6
Frank P <fpinder@comcast.net>
2019-09-18 14:35:05 -0700

    added DB and updated Mysql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers  (burger_name, devoured) VALUES ('Beef Burgers', false);
+INSERT INTO burgers  (burger_name, devoured) VALUES ('Turkey Burgers', false);
+INSERT INTO burgers  (burger_name, devoured) VALUES ('Veggie Burgers', false);
