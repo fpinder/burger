999711e9f234fa527646c5259d8160d577c7a3df
Frank P <fpinder@comcast.net>
2019-09-21 23:28:40 -0700

    added view layout and models

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+require("dotenv").config();
+
+var connection = mysql.createConnection({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+// Make connection.
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+
+module.exports = connection;
